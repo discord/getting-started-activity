4fb7bd70eebbc5734b83f6643ad970aabb3ce2a3
Colin Loretz <colin.loretz@discordapp.com>
2024-02-13 09:59:36 -0800

    activity tutorial template

---
+++ client/main.js
@@ -0,0 +1,9 @@
+import './style.css'
+import rocketLogo from '/rocket.png'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <img src="${rocketLogo}" class="logo" alt="Discord" />
+    <h1>Hello, World!</h1>
+  </div>
+`;
---
+++ server/server.js
@@ -0,0 +1,37 @@
+import express from "express";
+import dotenv from "dotenv";
+import fetch from "node-fetch";
+dotenv.config({ path: "../.env" });
+
+const app = express();
+const port = 3001;
+
+// Allow express to parse JSON bodies
+app.use(express.json());
+
+app.post("/api/token", async (req, res) => {
+  
+  // Exchange the code for an access_token
+  const response = await fetch(`https://discord.com/api/oauth2/token`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: new URLSearchParams({
+      client_id: process.env.VITE_DISCORD_CLIENT_ID,
+      client_secret: process.env.DISCORD_CLIENT_SECRET,
+      grant_type: "authorization_code",
+      code: req.body.code,
+    }),
+  });
+
+  // Retrieve the access_token from the response
+  const { access_token } = await response.json();
+
+  // Return the access_token to our client as { access_token: "..."}
+  res.send({access_token});
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
