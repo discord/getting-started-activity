ef928c66c150bc6ef470eee35ccba133305a0104
Colin Williams <colin.williams.dev@gmail.com>
2024-06-24 22:13:31 -0500

    feat: import and instantiate discordSDK obj

--- client/main.js
+++ client/main.js
@@ -1,7 +1,19 @@
-import './style.css'
-import rocketLogo from '/rocket.png'
+import { DiscordSDK } from "@discord/embedded-app-sdk";
 
-document.querySelector('#app').innerHTML = `
+import "./style.css"
+import rocketLogo from "/rocket.png"
+
+const discordSDK = new DiscordSDK(import.meta.env.VITE_DISCORD_CLIENT_ID);
+
+setupDiscordSDK().then(() => {
+  console.log("Discord SDK is ready.");
+});
+
+async function setupDiscordSDK() {
+  await discordSDK.ready();
+};
+
+document.querySelector("#app").innerHTML = `
   <div>
     <img src="${rocketLogo}" class="logo" alt="Discord" />
     <h1>Hello, World!</h1>
