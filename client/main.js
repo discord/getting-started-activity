e686bc050c7e183ff34be8eaf66a702a8c44477e
Colin Williams <colin.williams.dev@gmail.com>
2024-06-24 23:05:15 -0500

    feat: client side POST auth request

--- client/main.js
+++ client/main.js
@@ -3,15 +3,45 @@ import { DiscordSDK } from "@discord/embedded-app-sdk";
 import "./style.css"
 import rocketLogo from "/rocket.png"
 
+let auth;
+
+/* this is how we can access VITE_ prefixed env vars */
 const discordSDK = new DiscordSDK(import.meta.env.VITE_DISCORD_CLIENT_ID);
 
 setupDiscordSDK().then(() => {
-  console.log("Discord SDK is ready.");
+  console.log("Discord SDK is authenticated.");
 });
 
 async function setupDiscordSDK() {
+
   await discordSDK.ready();
+  console.log("Discord SDK is ready.");
+
+  /* authorize with Discord Client */
+  const { code } = await discordSDK.commands.authorize({
+    client_id: import.meta.env.VITE_DISCORD_CLIENT_ID,
+    response_type: "code",
+    state: "",
+    prompt: "none",
+    scope: ["identify", "guilds"]
+  });
+
+  /* retrieve an access_token from the activity's server */
+  const response = await fetch("/api/token", {
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify({ code })
+  });
+
+  const { access_token } = await response.json();
+
+  /* authenticate with Discord Client using the access_token */
+  auth = await discordSDK.commands.authenticate({ access_token });
+
+  if (auth == null) throw new Error("Authenticate command failed");
+
 };
+
 
 document.querySelector("#app").innerHTML = `
   <div>
